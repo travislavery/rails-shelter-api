97476a303eece9b4244ea257f9a20ae65a169450
TravisA <github email address>
2018-04-22 21:04:24 -0600

    add item_group to create

--- app/controllers/v1/items_controller.rb
+++ app/controllers/v1/items_controller.rb
@@ -12,7 +12,10 @@ class V1::ItemsController < ApplicationController
 		
 		shelter = Shelter.find_by(id: params[:shelter_id])
 		if current_user.id == shelter.user.id
+			@item_group = ItemGroup.find_or_initialize_by(name: params[:group_name])
 			@item = Item.find_or_initialize_by(item_params)
+			@item.item_group = @item_group
+			@item.save
 			shelter.items << @item
 			if shelter.save
 				render json: shelter, status: :created
